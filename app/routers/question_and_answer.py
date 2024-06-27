73b543ee0d7ee386a1d458c1546617b3998f2394
zzc0101 <318185326@qq.com>
2024-06-27 15:25:29 +0800

    update: 修改选择界面分页问题

--- app/routers/question_and_answer.py
+++ app/routers/question_and_answer.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Query
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from config.config import QA_TITLE
 
 router = APIRouter()
@@ -14,4 +14,14 @@ templates = Jinja2Templates(directory="templates/qa")
 async def read_root(request: Request):
     return templates.TemplateResponse("qaSelect.html", {"request": request, "data": QA_TITLE})
 
+
+# 响应问答标注的分页数据
+@router.get("/qa/data")
+async def read_data(page: int = Query(1, alias="page"), size: int = Query(10, alias="size")):
+    start = (page - 1) * size
+    end = start + size
+    data = []
+    paged_data = data[start:end]
+    page_count = len(data) / 10 if (len(data) / 10) > 0 else 1
+    return JSONResponse({"data": paged_data,"currentPage": page, "totalItems": len(data), "pageCount": page_count })
 
